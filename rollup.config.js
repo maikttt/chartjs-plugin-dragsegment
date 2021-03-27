d6fdd94def8c24675ae7bd7695b526e64fe0c055
Mihai Terenti <maikttt@gmail.com>
2021-03-27 12:18:52 +0200

    use  to minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,17 @@
 import {nodeResolve} from '@rollup/plugin-node-resolve';
+import {terser} from "rollup-plugin-terser";
 
 export default {
   input: './src/main.js',
   output: {
-    file: './dist/bundle.js',
+    file: './dist/chartjs-plugin-dragsegment.min.js',
     format: 'iife',
     sourcemap: false,
   },
-
   plugins: [
     nodeResolve({
       browser: true
-    })
+    }),
+    terser()
   ],
 };
